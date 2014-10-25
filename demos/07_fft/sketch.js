da5c00ab6c59a2f105e76238ec22c6a796f4d51b
Jason Sigal <jason.sigal@gmail.com>
2014-10-24 21:15:01 -0400

    tweaking fft example

--- demos/07_fft/sketch.js
+++ demos/07_fft/sketch.js
@@ -1,13 +1,14 @@
 /*
-  Analyze the frequency spectrum with FFT.
-  Particles represent bins of the FFT frequency spectrum. 
+  Analyze the frequency spectrum with FFT (Fast Fourier Transform)
+  Draw a 1024 particles system that represents bins of the FFT frequency spectrum. 
  */
 
-var mic, soundfile; // input sources
+var mic, soundfile; // input sources, press T to toggleInput()
+
 
 var fft;
-var smoothing = 0.8;
-var binCount = 1024;
+var smoothing = 0.8; // play with this, between 0 and .99
+var binCount = 1024; // size of resulting FFT array. Must be a power of 2 between 16 an 1024
 
 var particles =  new Array(binCount);
 
@@ -22,45 +23,82 @@ function setup() {
   soundfile.play();
   mic = new p5.AudioIn();
 
-  // FFT
-  var smoothing = 0.8; // play with this number, between 0 and 0.99
+  // initialize the FFT, plug in our variables for smoothing and binCount
   fft = new p5.FFT(smoothing, binCount);
 
   // instantiate the particles.
   for (var i = 0; i < particles.length; i++) {
-    var pos = createVector(
-      // x position corresponds with its position in the frequency spectrum
-      map(i, 0, 1023, 0, width),
+    var position = createVector(
+      // x position corresponds with position in the frequency spectrum
+      map(i, 0, binCount, 0, width * 2),
       random(0, height)
     );
-    particles[i] = new Particle(pos);
+    particles[i] = new Particle(position);
   }
 }
 
 function draw() {
+  // r, g, b, alpha
   background(0, 0, 0, 100);
 
-  var spectrum = fft.analyze();
+  // returns an array with [binCount] amplitude readings from lowest to highest frequencies
+  var spectrum = fft.analyze(binCount);
 
-  // update and draw all 1024 particles!
+  // update and draw all [binCount] particles!
   // Each particle gets a level that corresponds to
   // the level at one bin of the FFT spectrum. 
-  // This level is like amplitude, but usually called "energy."
+  // This level is like amplitude, often called "energy."
   // It will be a number between 0-255.
-  for (var i = 0; i < particles.length; i++) {
+  for (var i = 0; i < binCount; i++) {
     var thisLevel = map(spectrum[i], 0, 255, 0, 1);
     particles[i].update( thisLevel );
     particles[i].draw();
+
+    // update x position (in case we change the bin count)
+    particles[i].position.x = map(i, 0, binCount, 0, width * 2);
   }
+}
+
+// ===============
+// Particle class
+// ===============
+
+var Particle = function(position) {
+  this.position = position;
+  this.scale = random(0, 1);
+  this.speed = createVector(0, random(0, 10) );
+  this.color = [random(0, 255), random(0,255), random(0,255)];
+}
+
+var theyExpand = 1;
+
+// use FFT bin level to change speed and diameter
+Particle.prototype.update = function(someLevel) {
+  this.position.y += this.speed.y / (someLevel*2);
+  if (this.position.y > height) {
+    this.position.y = 0;
+  }
+  this.diameter = map(someLevel, 0, 1, 0, 100) * this.scale * theyExpand;
 
 }
+
+Particle.prototype.draw = function() {
+  fill(this.color);
+  ellipse(
+    this.position.x, this.position.y,
+    this.diameter, this.diameter
+  );
+}
+
+// ================
+// Helper Functions
+// ================
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   background(0);
 }
 
-// in p5, keyPressed is not case sensitive, but keyTyped is
 function keyPressed() {
   if (key == 'T') {
     toggleInput();
@@ -79,33 +117,4 @@ function toggleInput() {
     mic.stop();
     fft.setInput(soundfile);
   }
-}
-
-
-// ===============
-// Particle class
-// ===============
-
-var Particle = function(position) {
-  this.position = position;
-  this.scale = random(0, 5);
-  this.speed = random(0, 10);
-  this.color = [random(0, 255), random(0,255), random(0,255)];
-}
-
-// use FFT bin level to change speed and diameter
-Particle.prototype.update = function(someLevel) {
-  this.position.y += this.speed / (someLevel*2);
-  if (this.position.y > height) {
-    this.position.y = 0;
-  }
-  this.diameter = map(someLevel, 0, 1, 0, 100) * this.scale;
-}
-
-Particle.prototype.draw = function() {
-  fill(this.color);
-  ellipse(
-    this.position.x, this.position.y,
-    this.diameter, this.diameter
-  );
 }
